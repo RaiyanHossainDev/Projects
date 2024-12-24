f6a3f9a8897c98e7da7b2c5dab3a07236ae8efae
RaiyanHossainDev <astralfog2@gmail.com>
2024-12-24 11:53:54 +0600

    note6

--- Project 8 Note/note/src/store.js
+++ Project 8 Note/note/src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import  userSlice  from './slice/UserSlice.js'
+import  {userSlice}  from './slice/UserSlice'
 
 export default configureStore({
   reducer: {
